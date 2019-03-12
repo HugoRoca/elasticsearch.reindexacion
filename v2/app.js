36adaef160d4f8682e3b3b66db4d0b94d1ac460a
HugoRoca <hugo.rock20@hotmail.com>
2019-03-12 17:51:27 -0500

    more chages

--- v2/app.js
+++ v2/app.js
@@ -2,7 +2,7 @@
 
 const app = (function () {
 
-    var time = 0;
+    var reindexar = 0;
     var cronometro = 0;
 
     const _config = {
@@ -39,29 +39,29 @@ const app = (function () {
                 i += 1;
                 tabla += `<tr id="${element.index}" data-item="filaReindexación">`;
                 tabla += `<td><button class='btn btn-warning reindexar'>Reindexar</button></td>`;
-                tabla += `<td>
+                tabla += `<td style="vertical-align: middle">
                         <a href="javascript:;" class="star">
                             <img src='image/botonNaranja.png' class='media-photo imagen-cambiar' alt=''>
                         </a>
                     </td>`;
-                tabla += `<td>
+                tabla += `<td style="vertical-align: middle">
                         <div class="media">
                             <div class="media-body">
                                 <input data-item="id" type="hidden" value="${element.index}">
                                 <h3 class='title'>${element.index} 
-                                    <span class="pull-right pendiente">(Pendiente)</span>
+                                    <span class="pull-right label-small pendiente">(Pendiente)</span>
                                 </h3>
                             </div>
                         </div>
                     </td>`;
-                tabla += `<td>
+                tabla += `<td style="vertical-align: middle">
                         <div class="media">
                             <div class="media-body">
                                 <h4 class="title">Registros: ${element["docs.count"]}</h4>
                             </div>
                         </div>
                     </td>`;
-                tabla += `<td>
+                tabla += `<td style="vertical-align: middle">
                         <div class="media">
                             <div class="media-body">
                                 <h4 class="title">Peso: ${element["store.size"]}</h4>
@@ -69,8 +69,12 @@ const app = (function () {
                         </div>
                     </td>`;
 
-                tabla += `<td>
-                        <a id="cargando" class="start" href="javascript:;"></a>
+                tabla += `<td style="vertical-align: middle">
+                        <div class="media">
+                            <div class="media-body">
+                                <h4 class="title cronometro"></h4>
+                            </div>
+                        </div>
                     </td>`;
                 tabla += "</tr>";
 
@@ -84,24 +88,56 @@ const app = (function () {
     const _eventos = {
         reindexar: function (e) {
             e.preventDefault();
-
+            console.log("comenzo a reindexar");
+var i = 0;
             let padre = $(this).parents("[data-item='filaReindexación']").eq(0);
             let index = $(padre).find("[data-item='id']").val();
-            let imagen = $(padre).find(".imagen-cambiar");
-
             let logId = "#" + index + "_log";
             let newIndex = index.replace("producto_v1", "producto_v2");
             let log = `<strong>Hora de inicio ${obtenerHora()} → Creación de índice: ${newIndex}</strong><br>`;
             $(logId).html(`<td colspan="6"><p><small>${log}</small></p></td>`);
-            imagen.attr("src", "image/cargando.gif");
 
-            time = setInterval(() => {
+            _eventos.cambiosEstilosInicio(padre);
+
+            reindexar = setInterval(() => {
+                i++;
                 log += `Total Registros:`;
-                
+
                 let loghtml = `<td colspan="6"><p><small>${log}</small></p></td>`;
                 $(logId).html(loghtml);
+
+                if (i === 5){
+                    clearInterval(reindexar);
+                    _eventos.cambiosEstiloFin(padre);
+                }
             }, 1000);
-            
+
+        },
+
+        cambiosEstilosInicio: function (value) {
+            let imagen = $(value).find(".imagen-cambiar");
+            let cronometro = $(value).find(".cronometro");
+            let labelSmall = $(value).find(".label-small");
+            let buttonReindexar = $(value).find(".reindexar");
+
+            imagen.attr("src", "image/cargando.gif");
+            cronometro.html("Duración: 01:03:05");
+            cronometro.css({ "color": "green", "font-size": "25px" });
+            labelSmall.html("(Reindexando)");
+            labelSmall.css("color", "green");
+            buttonReindexar.prop("disabled", true);
+        },
+
+        cambiosEstiloFin: function (value) {
+            let imagen = $(value).find(".imagen-cambiar");
+            let cronometro = $(value).find(".cronometro");
+            let labelSmall = $(value).find(".label-small");
+            let buttonReindexar = $(value).find(".reindexar");
+
+            imagen.attr("src", "image/botonVerde.gif");
+            labelSmall.html("(Reindexado)");
+            buttonReindexar.text("Reindexado");
+            buttonReindexar.toggleClass("btn-warning btn-success")
         }
     }
 
